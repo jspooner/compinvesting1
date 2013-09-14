70b4f7bc126b5b54234db51bea8dfc20ecc47109
Jonathan Spooner <jspooner@gmail.com>
2013-09-14 10:04:48 -0700

    Working function, maybe...

--- Homework-1/part2.py
+++ Homework-1/part2.py
@@ -17,10 +17,6 @@ import numpy as np
 
 def simulate(dt_start, dt_end, ls_symbols, allocations):
     '''Simulate Function'''
-    vol            = 0
-    daily_ret      = 0
-    sharpe         = 0
-    cum_ret        = 0    
     dt_timeofday   = dt.timedelta(hours=16) # We need closing prices so the timestamp should be hours=16.
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday) # Get a list of trading days between the start and the end.
     c_dataobj      = da.DataAccess('Yahoo') # Creating an object of the dataaccess class with Yahoo as the source.
@@ -29,21 +25,27 @@ def simulate(dt_start, dt_end, ls_symbols, allocations):
     # Timestamps and symbols are the ones that were specified before.
     ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data   = dict(zip(ls_keys, ldf_data))
-    # Filling the data for NAN
-    for s_key in ls_keys:
-        d_data[s_key] = d_data[s_key].fillna(method='ffill')
-        d_data[s_key] = d_data[s_key].fillna(method='bfill')
-        d_data[s_key] = d_data[s_key].fillna(1.0)
     # Getting the numpy ndarray of close prices.
-    df_rets = d_data['close'].copy() 
-    df_rets = df_rets.fillna(method='ffill') 
-    df_rets = df_rets.fillna(method='bfill') 
-    df_rets = df_rets.fillna(1.0)
-    na_rets = df_rets.values
-    print("original stock values") 
-    print(df_rets['GOOG'])
-    
-    # print np.std([[0,0], [1,1]])
+    na_price = d_data['close'].values
+    print na_price
+    print "# Normalizing the prices to start at 1 and see relative returns"
+    na_normalized_price = na_price / na_price[0, :]
+    print na_normalized_price
+    print "# Multiply each column by the allocation to the corresponding equity."
+    na_normalized = na_normalized_price.copy()
+    na_price_alloc = na_normalized * allocations
+    print na_price_alloc
+    print "# Sum each row for each day. That's your cumulative daily portfolio value."
+    na_portfolio = np.sum(na_price_alloc, axis=1)
+    na_port = na_portfolio.copy()
+    print na_port
+    print "# Copy the normalized prices to a new ndarry to find returns."
+    na_ret = tsu.returnize0(na_port)
+    print "# Statistics from the total portfolio value."
+    daily_return = np.average(na_ret)
+    vol          = np.std(na_ret)
+    sharpe_ratio = ((252)**(.5))*(daily_return/vol)
+    cum_ret      = na_portfolio[len(na_portfolio)-1]
     
     return vol, daily_ret, sharpe, cum_ret
 
